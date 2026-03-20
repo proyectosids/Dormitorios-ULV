057e62810fccbc8c542bf31949d9b1bb62d277f4
Duem07 <emiliano.ortega@ulv.edu.mx>
2026-03-20 08:46:10 -0600

    implementacion de sharp

--- src/services/cloudinary.service.js
+++ src/services/cloudinary.service.js
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
+import sharp from 'sharp';
 
 // ⚙️ Configuración de Cloudinary usando variables de entorno
 // Asegúrate de que tu archivo .env tenga CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY y CLOUDINARY_API_SECRET
@@ -13,30 +14,25 @@ cloudinary.config({
  * @param {Buffer} fileBuffer - El buffer de la imagen recibida por Multer
  * @returns {Promise<{url: string, publicId: string}>} - URL y ID de la imagen
  */
+
 export const subirImagen = async (fileBuffer) => {
+  const bufferOptimizado = await sharp(fileBuffer)
+    .resize(800)
+    .jpeg({ quality:70 })
+    .toBuffer();
+
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
-      { 
-        folder: 'evidencias_limpieza',
-        transformation: [
-          { width: 800, quality: "auto", fetch_format: "auto" }
-        ] 
-      },
+      { folder: 'evidencias_limpieza' },
       (error, result) => {
-        if (error) {
-          console.error("❌ Error en Cloudinary upload_stream:", error);
-          return reject(error);
-        }
-        
-        // Retornamos ambos valores para que SISTEMA pueda borrar la foto después de 7 días
-        resolve({ 
+        if (error)  reject(error);
+          else resolve({ 
           url: result.secure_url, 
           publicId: result.public_id 
         });
       }
     );
-
     // Finaliza el stream enviando el buffer de la imagen
-    uploadStream.end(fileBuffer);
+    uploadStream.end(bufferOptimizado);
   });
 };
